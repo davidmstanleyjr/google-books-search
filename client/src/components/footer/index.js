cc6407f31c211d96effbf1cff5be5e6a8e680b77
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2020-01-08 21:25:30 -0500

    finished footer stuff

---
+++ client/src/components/footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+    <footer className="page-footer">
+        <div className="container-fluid text-center">
+            <p>David M. Stanley Jr.</p>
+        </div>
+
+    </footer>
+);
+
+export default Footer;
