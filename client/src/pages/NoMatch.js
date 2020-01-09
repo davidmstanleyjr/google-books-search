1542ec4baa0a2ac3689accbeb5f804d94655c5fb
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2020-01-09 00:46:14 -0500

    finished pages folder logic

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Col, Row, Container } from "../components/Grid";
+import Jumbotron from "../components/Jumbotron";
+
+//this is for when no match for a book is found
+function NoMatch() {
+    return (
+        <Container fluid>
+            <Row>
+                <Col size="md-12">
+                    <Jumbotron>
+                        <h1 className="text-white">404 Page Not Found</h1>
+                        <h1>
+                            <span role="img" aria-label="Face With Rolling Eyes Emoji">
+                                🙄
+              </span>
+                        </h1>
+                    </Jumbotron>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+
+export default NoMatch;
